168da4e02188a4adbc1a646eab8359fc8a8a5b67
Ewe Zu Lin <zlewe1997@gmail.com>
2020-05-11 19:10:52 +0800

    add docker

---
+++ docker_run.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+ARGS=("$@")
+
+# Make sure processes in the container can connect to the x server
+# Necessary so gazebo can create a context for OpenGL rendering (even headless)
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]; then
+    xauth_list=$(xauth nlist $DISPLAY)
+    xauth_list=$(sed -e 's/^..../ffff/' <<<"$xauth_list")
+    if [ ! -z "$xauth_list" ]; then
+        echo "$xauth_list" | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+
+# Prevent executing "docker run" when xauth failed.
+if [ ! -f $XAUTH ]; then
+    echo "[$XAUTH] was not properly created. Exiting..."
+    exit 1
+fi
+
+BASH_OPTION=bash
+
+docker run -it \
+    -e DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -e XAUTHORITY=$XAUTH \
+    -v "/home/$USER/duckietown:/home/hcc2020/duckietown" \
+    -v "$XAUTH:$XAUTH" \
+    -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+    -v "/etc/localtime:/etc/localtime:ro" \
+    -v "/dev:/dev" \
+    -v "/var/run/docker.sock:/var/run/docker.sock" \
+    --name duckietop \
+    --network host \
+    --rm \
+    --privileged \
+    --security-opt seccomp=unconfined \
+    hamesewe/duckietop:kinetic \
+    $BASH_OPTION
